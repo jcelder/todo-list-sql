778f0033a3d439cf16f6ac3242cbe5293cbcb29a
Joshua Elder <aresx130@gmail.com>
2017-10-12 10:54:12 -0700

    Added Postgres server information, connect statements, and data handling via promises.

--- dbOperations.js
+++ dbOperations.js
@@ -1,1 +1,23 @@
-const client = require('pg')
+const { Client } = require('pg')
+
+const client = new Client({
+  user: 'test',
+  host: 'localhost',
+  database: 'todo-list',
+  password: '1234',
+  port: 5432
+})
+
+const accessDB = (query) => {
+  let returnData;
+  client.connect()
+
+  return client.query(query)
+    .then((result) => {
+      client.end()
+      return result.rows
+    })
+    .catch(err => console.error(err.message))
+}
+
+module.exports = { accessDB }
