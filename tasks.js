5b375865dfc57f2daf12886f58230bc4eef7c412
Joshua Elder <aresx130@gmail.com>
2017-10-12 15:27:17 -0700

    Added switch statement for argument handling.

--- tasks.js
+++ tasks.js
@@ -2,3 +2,28 @@
 
 const command = process.argv[2]
 const task = process.argv.slice(3).join(' ')
+const list = require('./commands/list').list
+const add = require('./commands/add').add
+const remove = require('./commands/delete').remove
+const complete = require('./commands/complete').complete
+
+
+switch (command) {
+  case 'list':
+    list()
+    // process.exit(0)
+    break;
+  case 'add':
+    add(task)
+    // process.exit(0)
+    break;
+  case 'complete':
+    complete(task)
+    // process.exit(0)
+    break;
+  case 'delete':
+    remove(task)
+    break;
+  default:
+    console.log('Test')
+}
